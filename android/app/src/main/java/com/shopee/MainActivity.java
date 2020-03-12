c03a8700c9f83ed5a77cf1a03164245903ca7c5b
heyanqiu <heyanqiu@zhubajie.com>
2020-03-12 20:54:42 +0800

    React Navigation 4.x config setting

--- android/app/src/main/java/com/shopee/MainActivity.java
+++ android/app/src/main/java/com/shopee/MainActivity.java
@@ -1,6 +1,16 @@
+/*
+ * @description: react-navigation config. detail info:
+ *               https://reactnavigation.org/docs/4.x/getting-started/
+ * @author: tracyqiu
+ * @LastEditors: tracyqiu
+ * @LastEditTime: 2020-03-12 20:53:19
+ */
 package com.shopee;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +21,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "shopee";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
---
+++ src/utils/perfomance.ts
@@ -0,0 +1,51 @@
+/*
+ * @description: 防抖、截流等共工具函数
+ * @author: tracyqiu
+ * @LastEditors: tracyqiu
+ * @LastEditTime: 2020-03-10 16:44:26
+ */
+
+// tslint:disable:no-any
+/**
+ * 防抖函数, 回调函数只执行一次，但期间若有事件触发，则重新计时
+ * @param fn 最终执行的fn
+ * @param delay 延迟时间，单位ms
+ */
+export function debounce<T extends Function>(fn: T, delay: number = 300): T {
+  let timer: any;
+
+  return function() {
+    const args = arguments;
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  } as any;
+}
+
+/**
+ * 节流函数，一定时间内回调函数只执行一次。
+ * @param fn 最终执行的fn
+ * @param during 执行间隔时间，单位ms
+ */
+export function throttle<T extends Function>(fn: T, during: number = 200): T {
+  let start: number;
+  let timer: any;
+
+  return function() {
+    const context = this;
+    const args = arguments;
+
+    let now = +new Date();
+    if (now - start < during) {
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        start = now;
+        fn.apply(context, args);
+      }, during);
+    } else {
+      start = now;
+      fn.apply(context, args);
+    }
+  } as any;
+}
